07f1536a2f6e23e7c91445950ce7a7699be348d9
David Bernstein <david.bernstein@tufts.edu>
2016-03-06 10:49:26 -0500

    tried and failed to get data from herokuapp

--- messages/lab.js
+++ messages/lab.js
@@ -1,16 +1,21 @@
 // Your JavaScript goes here...
 function parse() {
+        document.domain = "messagehub.herokuapp.com";
         i = 0;
         var request = new XMLHttpRequest();
-        var url = "http://localhost:8000/data.json";
+        var url = "https://messagehub.herokuapp.com/messages.jsondata.json";
         request.open("GET", url, true);
         //console.log(request);
         request.send(null);
-console.log(request);
+        console.log(request);
+
         request.onreadystatechange = function() {
+                console.log("change");
+                console.log(request);
                 if(request.readyState == 4 && request.status == 200 && i ==0) {
                         data = JSON.parse(request.responseText);
                         for(var j = 0; j < data.length; j++) {
+                                console.log("got data");
                                 curr = document.getElementById("messages").innerHTML;
                                 document.getElementById("messages").innerHTML = 
                                 curr + "<br>" + "<h6 id='individualMsg'>" + data[j].content + " - " + data[j].username+ "</h6>";

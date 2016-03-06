241b8de81f7f2020e3f915888c8137be02a01cad
David Bernstein <david.bernstein@tufts.edu>
2016-03-05 23:55:45 -0500

    added JSON parser, css stylings

--- messages/lab.js
+++ messages/lab.js
@@ -1,1 +1,24 @@
 // Your JavaScript goes here...
+function parse() {
+        i = 0;
+        var request = new XMLHttpRequest();
+        var url = "http://localhost:8000/data.json";
+        request.open("GET", url, true);
+        //console.log(request);
+        request.send(null);
+console.log(request);
+        request.onreadystatechange = function() {
+                if(request.readyState == 4 && request.status == 200 && i ==0) {
+                        data = JSON.parse(request.responseText);
+                        for(var j = 0; j < data.length; j++) {
+                                curr = document.getElementById("messages").innerHTML;
+                                document.getElementById("messages").innerHTML = 
+                                curr + "<br>" + "<h6 id='individualMsg'>" + data[j].content + " - " + data[j].username+ "</h6>";
+
+                        }       
+                        i++;
+                }
+        }
+
+
+}

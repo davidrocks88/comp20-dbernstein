a733a5886d1a734f5eace0ddeb327a1aecb2a3ba
David Bernstein <david.bernstein@tufts.edu>
2016-04-04 18:41:14 -0400

    added node mongo integration

---
+++ server3/index.js
@@ -0,0 +1,85 @@
+// Initialization
+var express = require('express');
+
+// Required if we need to use HTTP query or post parameters
+var bodyParser = require('body-parser');
+var validator = require('validator'); // See documentation at https://github.com/chriso/validator.js
+var app = express();
+// See https://stackoverflow.com/questions/5710358/how-to-get-post-query-in-express-node-js
+app.use(bodyParser.json());
+// See https://stackoverflow.com/questions/25471856/express-throws-error-as-body-parser-deprecated-undefined-extended
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Mongo initialization and connect to database
+// process.env.MONGOLAB_URI is the environment variable on Heroku for the MongoLab add-on
+// process.env.MONGOHQ_URL is the environment variable on Heroku for the MongoHQ add-on
+// If environment variables not found, fall back to mongodb://localhost/nodemongoexample
+// nodemongoexample is the name of the database
+var mongoUri = process.env.MONGOLAB_URI || process.env.MONGOHQ_URL || 'mongodb://localhost/nodemongoexample';
+var MongoClient = require('mongodb').MongoClient, format = require('util').format;
+var db = MongoClient.connect(mongoUri, function(error, databaseConnection) {
+        db = databaseConnection;
+});
+
+// Serve static content
+app.use(express.static(__dirname + '/public'));
+
+app.post('/sendLocation', function(request, response) {
+        var login = request.body.login;
+        var lat = request.body.lat;
+        var lng = request.body.lng;
+
+        console.log(login);
+        console.log(lat);
+        console.log(lng);
+        /*
+        db.collection('fooditems', function(error, coll) {
+                var id = coll.insert(toInsert, function(error, saved) {
+                        if (error) {
+                                response.send(500);
+                        }
+                        else {
+                                response.send(200);
+                        }
+            });
+        });*/
+});
+app.post('/feedme', function(request, response) {
+        var food = request.body.food;
+        food = food.replace(/[^\w\s]/gi, '');
+        var toInsert = {
+                "food": food,
+        };
+        db.collection('fooditems', function(error, coll) {
+                var id = coll.insert(toInsert, function(error, saved) {
+                        if (error) {
+                                response.send(500);
+                        }
+                        else {
+                                response.send(200);
+                        }
+            });
+        });
+});
+
+app.get('/', function(request, response) {
+        response.set('Content-Type', 'text/html');
+        var indexPage = '';
+        db.collection('fooditems', function(er, collection) {
+                collection.find().toArray(function(err, cursor) {
+                        if (!err) {
+                                indexPage += "<!DOCTYPE HTML><html><head><title>What Did You Feed Me?</title></head><body><h1>What Did You Feed Me?</h1>";
+                                for (var count = 0; count < cursor.length; count++) {
+                                        indexPage += "<p>You fed me " + cursor[count].food + "!</p>";
+                                }
+                                indexPage += "</body></html>"
+                                response.send(indexPage);
+                        } else {
+                                response.send('<!DOCTYPE HTML><html><head><title>What Did You Feed Me?</title></head><body><h1>Whoops, something went terribly wrong!</h1></body></html>');
+                        }
+                });
+        });
+});
+
+// Oh joy! http://stackoverflow.com/questions/15693192/heroku-node-js-error-web-process-failed-to-bind-to-port-within-60-seconds-of
+app.listen(process.env.PORT || 3000);

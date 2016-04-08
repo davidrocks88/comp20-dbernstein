7609ca1827e6869e979ffdc90b8ca01c5d8c8399
David Bernstein <david.bernstein@tufts.edu>
2016-04-08 15:59:22 -0400

    cleaned up code

--- server3/index.js
+++ server3/index.js
@@ -23,19 +23,21 @@ var db = MongoClient.connect(mongoUri, function(error, databaseConnection) {
 
 // Serve static content
 app.use(express.static(__dirname + '/public'));
+
+// Enabling CORS
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
 });
+
 app.post('/sendLocation', function(request, response) {
-        // response.header("Access-Control-Allow-Origin", "*");
-        // response.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
         var login = request.body.login;
         var lat = Number(request.body.lat);
         var lng = Number(request.body.lng);
         var created_at = Date();
 
+        // Check to see if we got all the necessary data from the client
         if(!login || !lat || !lng) {
             response.send({"error":"Whoops, something is wrong with your data!"});
             return;
@@ -44,30 +46,34 @@ app.post('/sendLocation', function(request, response) {
         var returnData = new Object;
 
         var checkIn = {
-                "login"     : login,
-                "lat"       : lat,
-                "lng"       : lng,
-                "created_at": created_at
+            "login"     : login,
+            "lat"       : lat,
+            "lng"       : lng,
+            "created_at": created_at
         }
-        console.log(checkIn);
+
         db.collection('checkins', function(error, coll) {
+            // Insert the check in into the mongo database
             coll.insert(checkIn, function(error, saved) {
                 if (error) {
-                    console.log("error adding checkin");
+                    console.log("error adding check in");
                 }
                 else {
-                    console.log("Successfully added checkin");
+                    console.log("Successfully added check in");
                 }
             });
+
+            // Get all of the people from the check ins collection
             coll.find().toArray(function(peopleErr, people) {
                 returnData.people = people;
             });
         });
 
-
         db.collection('landmarks').createIndex({'geometry':"2dsphere"}, function(err, res){
             if(!err) {
                 db.collection('landmarks',function(error, coll) {
+
+                    // Indexing the landmarks
                     coll.find({geometry:{$near:{$geometry:{type:"Point",coordinates:[lng,lat]},$maxDistance: 1609}}}).toArray(function(landmarkErr, nearestLandmarks) {
                         if(!landmarkErr) {
                             returnData.landmarks = nearestLandmarks;
@@ -79,36 +85,18 @@ app.post('/sendLocation', function(request, response) {
                             response.send(200);
                         }
                     });
-            });
+                });
             }
             else {
-                console.log("error");
+                console.log("error creating landmarks index");
             }
-        });
-        // db.collection('landmarks', function(error, coll) {
-        //     coll.find({geometry:{$near:{$geometry:{type:"Point",coordinates:[lng,lat]},$minDistance: 0,$maxDistance: 1609}}}).toArray(function(err, cursor) {
-        //         console.log(cursor);
-        //         response.send(200);
-        //     });
-        // });
-        // db.collection('landmarks').find().toArray(function(err, cursor) {
-        //     if(!err) {
-        //         console.log(cursor);
-        //     }
-        //     else {
-        //         console.log('db error');
-        //     }
-        // });
-
-        
+        });   
 });
 
 app.get('/checkins.json', function(request, response) {
-        response.header("Access-Control-Allow-Origin", "*");
-        response.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-        
         var login = request.query.login;
 
+        // Check for login
         if(!login) {
             response.send([]);
             return;
@@ -116,37 +104,43 @@ app.get('/checkins.json', function(request, response) {
 
         var toFind = {"login": login};
 
+        // Find al check ins for the given login
         db.collection('checkins', function(error, coll) {
-                coll.find(toFind).toArray(function(err, cursor) {
-                        if (!err) {
-                                response.send(cursor);
-                        } else {
-                                console.log("error");
-                        }
-                });
+            coll.find(toFind).toArray(function(err, cursor) {
+                if (!err) {
+                    response.send(cursor);
+                } else {
+                    console.log("error");
+                }
+            });
         });
 });
 
 app.get('/', function(request, response) {
-        response.set('Content-Type', 'text/html');
-        var indexPage = '<!DOCTYPE HTML><html><head><title>Checkins</title></head><body><h1> Checkins, as hosted by David Bernstein </h1> <h3> Comp 20 - Assignment 3 </h3> <h4> CHECKINS </h4><ul>';
-        db.collection('checkins', function(er, collection) {
-                collection.find().sort({"created_at":1}).toArray(function(err, checkins) {
-                        if (!err) {
-                                for(var i = 0; i < checkins.length; i++) {
-                                    indexPage += '<li>' + checkins[i].login + " checked in at " + checkins[i].created_at + '</li>';
-                                    indexPage += '<ul> <li> Latitude: ' + checkins[i].lat + '</li>';
-                                    indexPage += '<li> Longitude: ' + checkins[i].lng + '</li></ul>';
-                                }
-                                indexPage += '</ul></body></html>';
-                                response.status(200);
-                                response.send(indexPage);
-                        } else {
-                                response.send('<!DOCTYPE HTML><html><head><title>What Did You Feed Me?</title></head><body><h1>Whoops, something went terribly wrong!</h1></body></html>');
-                        }
-                });
+    response.set('Content-Type', 'text/html');
+
+    // Header for the html page
+    var indexPage = '<!DOCTYPE HTML><html><head><title>Check ins</title></head><body><h1> Check ins, as hosted by David Bernstein </h1> <h3> Comp 20 - Assignment 3 </h3> <h4> CHECK INS </h4><ul>';
+    
+    // Get all the check ins from the mongo db
+    db.collection('checkins', function(er, collection) {
+        // Sort them so that the most recent check in is first
+        collection.find().sort({"created_at":1}).toArray(function(err, checkins) {
+            if (!err) {
+                // Add all the checkins to the html page
+                for(var i = 0; i < checkins.length; i++) {
+                    indexPage += '<li>' + checkins[i].login + " checked in at " + checkins[i].created_at + '</li>';
+                    indexPage += '<ul> <li> Latitude: ' + checkins[i].lat + '</li>';
+                    indexPage += '<li> Longitude: ' + checkins[i].lng + '</li></ul>';
+                }
+                indexPage += '</ul></body></html>';
+                response.status(200);
+                response.send(indexPage);
+            } else {
+                response.send('<!DOCTYPE HTML><html><head><title>What Did You Feed Me?</title></head><body><h1>Whoops, something went terribly wrong!</h1></body></html>');
+            }
         });
+    });
 });
 
-// Oh joy! http://stackoverflow.com/questions/15693192/heroku-node-js-error-web-process-failed-to-bind-to-port-within-60-seconds-of
 app.listen(process.env.PORT || 3000);
